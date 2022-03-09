eb79f7cc0937071b5d7f7fadd8fc11becd2019ca
zhangsan33 <8546260+zhangsan3356@user.noreply.gitee.com>
2022-03-09 08:35:49 +0800

    create a protocol struct to abstract the object

--- protocol/proto.go
+++ protocol/proto.go
@@ -1,1 +1,14 @@
 package protocol
+
+type Protocol struct {
+	FrameHeader   [4]byte
+	MessageHeader [8]byte
+	MessageBody   *[]byte
+	FrameTail     [4]byte
+}
+
+func NewProtocol(frameHeader [4]byte,
+	messageHeader [8]byte, messageBody *[]byte, frameTail [4]byte) *Protocol {
+	return &Protocol{FrameHeader: frameHeader, MessageHeader: messageHeader,
+		MessageBody: messageBody, FrameTail: frameTail}
+}
